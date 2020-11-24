6ef2204976cd3c4c5b1e325f1ffd22267a8881cf
UnfinishedProgram <noah.labrecque@gmail.com>
2020-11-24 15:55:23 -0500

    added universal navbar

---
+++ site/navbar.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    document.getElementById("navbar").innerHTML = 
+    `
+    <img src="images/logo.png"> <div id="nav_spacer"></div>
+    <a href="index.html">Home</a>
+    <a href="gameList.html">Find a Match</a>
+    <a href="index.html">Ongoing Matches</a>
+    <a href="index.html">About</a>
+    `
+};
